f67aef22ca58f690c79feed90bfa8982332cb3ab
Amna-mubasu123 <amnamubasuofficial@gmail.com>
2021-09-23 13:02:25 +0530

    mygit

---
+++ src/test/java/com/comcast/contact/ContactTest.java
@@ -0,0 +1,18 @@
+package com.comcast.contact;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test	
+	public void	createContactTest() {
+		System.out.println("Execute createContactTest");	
+	}
+	@Test
+	public void	createContactWithOrgTest() {
+		System.out.println("Execute createContactWithOrgTest");	
+	}
+	@Test
+	public void	deleteContactTest() {
+		System.out.println("Execute deleteContactTest");	
+	}
+}
